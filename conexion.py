bc21a7d70794f0ec54d1db018f288324677d29a5
Francisco Contreras <66226740+franciscocontreras93@users.noreply.github.com>
2020-11-28 18:39:15 -0400

    Update conexion.py

--- conexion.py
+++ conexion.py
@@ -3,14 +3,12 @@ from config import Configuracion
 
 class Conexion:
     def __init__(self):       
-        self.modelo = Configuracion()
-        self.loginStatus = False       
-        print(self.loginStatus)
+        self.modelo = Configuracion()        
+        self.loginStatus = False    
         self.idUser = None
         self.usuario = input("Ingrese su Usuario: ")
         self.password = input("Ingrese su Password: ")
-        self.login()
-        #print(self.idUser)
+        self.login()        
         try:
             self.accion(self.idUser)
         except psycopg2.Error as e:
@@ -22,59 +20,49 @@ class Conexion:
         self.modelo = Configuracion()
         self.modelo.create()
     
-    def login(self):
-        
-        modelo = Configuracion()
-        params = modelo.config()
-        print("Intentando Conectar...")
-        try:           
-            conn = modelo.conexionPrimaria()
-            cur = conn.cursor()
-            # UNCOMENT FOR DEBUG 
-            #print("La version de BD es: ")
+    def login(self):    
+        try:
+            print("Intentando Conectar...")
+            conn = self.conexionPrimaria()
+            cursor = conn.cursor()                      
             query = """SELECT * FROM cmg.cmg_usuarios_sstma WHERE id_usuario = '%s'  AND clave_qgis = '%s'""" % (self.usuario, self.password)
-            cur.execute(query)
-            rows = cur.fetchall()
-            result = cur.rowcount
-            
+            cursor.execute(query)
+            rows = cursor.fetchall()
+            result = cursor.rowcount            
             if result == 1 and self.loginStatus== False:
-
                 for row in rows: 
                     self.idUser = row[17] # PASARLO COMO PARAMETRO A LA CONEXION 2
                     nameUser = row[9]
                     lastnameUser = row[1]
-
-                print("Conexion Exitosa")
-                print("Bienvenido ", nameUser, lastnameUser)
-                #print(result)
+                print("Conexion Exitosa Bienvenido")                              
                 self.loginStatus = True
-                modelo.modifyUser(self.idUser) 
-                #print(self.loginStatus)
-                           
-            else:
-                #print(result)
+            else: 
                 print("Usuario o Password Incorrecta")
-            
-            
-            return self.loginStatus
-                       
-        except:
+            return self.loginStatus               
+        except psycopg2.Error as e:            
+            error_code = e.pgcode
+            print(error_code)
             print("Ocurrio un error en la Conexion")
         
     def accion(self, userDb):
         modelo = Configuracion()
         params = modelo.config()
-        
-        if self.loginStatus != False:
-                    
+        if self.loginStatus != False:      
             conn = modelo.conexionSecundaria(userDb)
-            cursor = conn.cursor()
-            cursor.execute("SELECT id_usuario FROM cmg.cmg_usuarios_sstma")
-            resultado = cursor.fetchall()          
+            cur = conn.cursor()
+            cur.execute("SELECT id_usuario FROM cmg.cmg_usuarios_sstma")
+            resultado = cur.fetchall()          
             for elemento in resultado:         
                 print(elemento)       
         
-     
+    def conexionPrimaria(self):
+        stringConexion = "dbname='prototipo' user='cmg_toolbox' host='181.143.104.82' password='23826405'"
+        try:            
+            conn = psycopg2.connect(stringConexion)
+            self.conexionStatus = True            
+        except:
+            print("Imposible Conectar")
+        return conn
 
 model = Conexion()
 
